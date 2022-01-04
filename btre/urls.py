b2fa0b45afa56bbf1510499dcec46154120ca2bb
Shiyang Chen <ana.chensy@gmail.com>
2022-01-04 16:49:19 -0500

    setup the pages with testing messages

--- btre/urls.py
+++ btre/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
